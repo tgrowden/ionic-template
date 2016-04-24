442f03cf6e9ff5e770120c66643206c808d35cee
tgrowden <taylorgrowden@gmail.com>
2016-04-24 18:42:56 -0400

    update

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,20 +1,18 @@
 angular.module('starter.controllers', [])
-
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
-
-  $scope.title = "Foobar";
-  $scope.externalLink = function(url) {
-    window.open(url, '_self', 'location=yes'); 
-    return false;
-  };
-  $scope.social = [
-    {
-      name: "Linkedin",
-      url: "https://www.linkedin.com/in/barrydandrea"
-    },
-    {
-      name: "Twitter",
-      url: "https://twitter.com/SalemeInsServ"
-    }
-  ];
-});
+  .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+    $scope.title = "Foobar"; // Edit
+    $scope.externalLink = function(url) {
+      window.open(url, '_self', 'location=yes');
+      return false;
+    };
+    $scope.social = [
+      {
+        name: "Linkedin",
+        url: "#"
+      },
+      {
+        name: "Twitter",
+        url: "#"
+      }
+    ];
+  });
